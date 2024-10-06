b216caf31f408247e5254a4fa75539311d66c5c6
Ashikur_Rahman <ashikurrahman7134@gmail.com>
2024-10-06 22:47:08 +0600

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Hash;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function authLogin(Request $request)
+    {
+        
+        $data = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+            
+        $remember = !empty($request->remember) ? true : false;
+        if (Auth::attempt($data, $remember)) {
+            return redirect('panel/dashboard');
+        } else {
+            return redirect()->back()->with('error', 'Please valid email and password.');
+        }
+    }
+}
