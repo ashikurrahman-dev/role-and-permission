f39bdeb4bc31e4e445816cc5929274b59e88cac1
Ashikur_Rahman <ashikurrahman7134@gmail.com>
2024-10-13 22:42:50 +0600

    dashboard setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class, 'login']);
 Route::post('/', [AuthController::class, 'authLogin']);
+
+
+Route::get('/panel/dashboard', [DashboardController::class, 'dashboard']);
